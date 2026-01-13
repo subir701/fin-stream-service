e6cbe8686deb076a037c90d21ea8b305996336fe
subir701 <subiradhikari181@gmail.com>
2026-01-13 20:50:38 +0530

    feat(config): complete r2dbc/redis config and docker-compose infrastructure (#1)

---
+++ fin-stream-service/src/test/java/com/fin_stream_service/FinStreamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fin_stream_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinStreamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
